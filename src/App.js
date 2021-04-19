1f3ec08dc560a69d7591bd84e78713904496b4b2
Shubham Singh Chahar <thefallenmerc@gmail.com>
2021-04-20 00:34:54 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h1 className="card-header">Create account</h1>
+        <div className="signup-holder">
+          Already have an account?
+           <a href="/login">Sign in</a>
+        </div>
+        <div className="form">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Full name" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control half" placeholder="First name" />
+            <input type="text" className="form-control half" placeholder="Last name" />
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" placeholder="E-mail" />
+          </div>
+          <button className="submit-button">Sign up</button>
+          <label className="terms">
+            <input type="checkbox" /> I have read and agreed to the <a href="/terms-n-conditions">Terms of Service</a>
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
